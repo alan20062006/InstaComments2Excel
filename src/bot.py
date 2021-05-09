7f767174c27341f98bbb7575ba7dba3ce327230b
alan20062006 <alan20062006@vip.qq.com>
2021-05-09 00:29:46 -0700

    add basic structure

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import traceback
 import yaml
+import numpy.random as random
 
 with open(r'config.yml') as file:
     account = yaml.full_load(file)
@@ -14,14 +15,22 @@ with open(r'config.yml') as file:
 class InstagramBot():
     def __init__(self, account):
         options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument('--headless')
+        # options.add_argument('--no-sandbox')
+        # options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=options)
         self.driver.implicitly_wait(5)
 
         self.username = account['username']
         self.password = account['password']
+    
+    def wait(self, mu = 2, sigma = 0.5):
+        # random wait time to avoid being blocked
+        t = random.normal(mu, sigma,1)[0]
+        if t <= 0:
+            return 0.1
+        else:
+            return t
     
     def signIn(self):
         self.driver.get('https://www.instagram.com/accounts/login/')
@@ -33,14 +42,60 @@ class InstagramBot():
             traceback.print_exc()
             print(e)
             return
-        import pdb;pdb.set_trace()
         self.driver.find_element_by_xpath("//input[@name='username']").send_keys(self.username)
-        time.sleep(1)
+        time.sleep(self.wait())
         self.driver.find_element_by_xpath("//input[@name='password']").send_keys(self.password)
-        time.sleep(2)
-        self.driver.find_element_by_xpath("//button[contains(.,'Log In')]").click()
-        time.sleep(3)
+        time.sleep(self.wait(mu=0.6))
+        self.driver.find_element_by_xpath("//input[@name='password']").send_keys(Keys.ENTER)
+        time.sleep(self.wait())
+
+        # Save Info or Not
+        self.driver.find_element_by_xpath("//button[contains(.,'保存信息')]").click()
+        time.sleep(self.wait())
+
+    def dive(self, username: str, depth=0, max_depth=0):
+        if depth > max_depth:
+            return 
+        self.get_profile(username)
+        followings = self.get_followings(username, 0)
+        for f in followings:
+            self.dive(f"https://www.instagram.com/{f}/", depth+1, max_depth)
+
+    
+    def get_profile(self, username: str):
+        url = f'https://www.instagram.com/{username}/'
+        return
+        self.driver.get(root_url)
+
+
+    def get_followings(self, username: str, max: int):
+        url = f'https://www.instagram.com/{username}/'
+        self.driver.get(url)
+        import pdb;pdb.set_trace()
+        followersLink = self.driver.find_element_by_css_selector('ul li a')
+        followersLink.click()
+        self.wait()
+        followersList = self.driver.find_element_by_css_selector('div[role=\'dialog\'] ul')
+        numberOfFollowersInList = len(followersList.find_elements_by_css_selector('li'))
+    
+        followersList.click()
+        actionChain = webdriver.ActionChains(self.driver)
+        while (numberOfFollowersInList < max):
+            actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+            numberOfFollowersInList = len(followersList.find_elements_by_css_selector('li'))
+            print(numberOfFollowersInList)
+        
+        followers = []
+        for user in followersList.find_elements_by_css_selector('li'):
+            userLink = user.find_element_by_css_selector('a').get_attribute('href')
+            print(userLink)
+            followers.append(userLink)
+            if (len(followers) == max):
+                break
+        return followers
+
 
 if __name__ == "__main__":
     ib = InstagramBot(account)
     ib.signIn()
+    ib.dive('wefluens', 0, 0)
